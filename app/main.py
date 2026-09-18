ce05823c057bbcafecfad682549344c8022df0c5
JohnRoss Doherty <dohertyjr0@icloud.com>
2026-09-18 10:56:42 +0000

    Added Route for Adding User

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from app.schemas import UserCreate
 
 app = FastAPI(title = "Lab 1 - FastAPI User API")
+
+users: list[UserCreate] = []
 
 @app.get("/health")
 def health():
@@ -10,3 +13,7 @@ def health():
 def hello():
     return{"message": "Hello from FastAPI"}
 
+@app.post("/api/users", status_code = status.HTTP_201_CREATED)
+def add_user(new_user: UserCreate):
+    users.append(new_user)
+    return new_user
