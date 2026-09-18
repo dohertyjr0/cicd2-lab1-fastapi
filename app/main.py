b6a014ecd9b106eec48583cdefc5d8932c8f69ad
JohnRoss Doherty <dohertyjr0@icloud.com>
2026-09-18 11:15:40 +0000

    Added GET return list and return list UserID

--- app/main.py
+++ app/main.py
@@ -15,5 +15,24 @@ def hello():
 
 @app.post("/api/users", status_code = status.HTTP_201_CREATED)
 def add_user(new_user: UserCreate):
+    for existing_user in users:
+        if existing_user.user_id == new_user.user_id:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail = "A User with this ID exists",)
     users.append(new_user)
     return new_user
+
+@app.get("/api/users")
+def get_users():
+    return users
+
+@app.get("/api/users/{users_id}")
+def get_user(user_id: int):
+    for existing_user in users:
+        if existing_user.user_id == user_id:
+            return existing_user
+
+    raise HTTPException(
+        status_code = status.HTTP_404_NOT_FOUND,
+        detail = "User not Found",
+    )
